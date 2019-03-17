e603c5c1470a6487387b78a695cea88afab34282
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-17 22:45:53 +0100

    Portapps 1.20.3

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 	. "github.com/portapps/portapps"
 	"github.com/portapps/portapps/pkg/proc"
-	"github.com/portapps/portapps/pkg/win"
+	"github.com/portapps/portapps/pkg/utl"
 )
 
 type config struct {
@@ -28,87 +28,98 @@ type machine struct {
 	OnExitRemove bool   `yaml:"on_exit_remove" mapstructure:"on_exit_remove"`
 }
 
-var cfg = config{
-	Machine: machine{
-		Name:         "default",
-		HostCIDR:     "192.168.99.1/24",
-		CPU:          1,
-		Ram:          1024,
-		Disk:         20000,
-		SharedName:   "shared",
-		OnExitStop:   false,
-		OnExitRemove: false,
-	},
-}
+var (
+	app *App
+	cfg *config
+)
 
 func init() {
-	Papp.ID = "docker-toolbox-portable"
-	Papp.Name = "Docker Toolbox"
-	InitWithCfg(&cfg)
+	var err error
 
-	win.SetConsoleTitle(fmt.Sprintf("%s Portable", Papp.Name))
+	// Default config
+	cfg = &config{
+		Machine: machine{
+			Name:         "default",
+			HostCIDR:     "192.168.99.1/24",
+			CPU:          1,
+			Ram:          1024,
+			Disk:         20000,
+			SharedName:   "shared",
+			OnExitStop:   false,
+			OnExitRemove: false,
+		},
+	}
+
+	// Init app
+	if app, err = NewWithCfg("docker-toolbox-portable", "Docker Toolbox", cfg); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
 }
 
 func main() {
-	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = CreateFolder(AppPathJoin("data"))
-	Papp.Process = PathJoin(Papp.AppPath, "git", "bin", "bash.exe")
-	Papp.Args = []string{
+	utl.CreateFolder(app.DataPath)
+	app.Process = utl.PathJoin(app.AppPath, "git", "bin", "bash.exe")
+	app.Args = []string{
 		"--login",
 		"-i",
-		PathJoin(Papp.AppPath, "start.sh"),
+		utl.PathJoin(app.AppPath, "start.sh"),
 	}
-	Papp.WorkingDir = Papp.AppPath
 
-	sharedPath := CreateFolder(PathJoin(Papp.DataPath, "shared"))
-	storagePath := CreateFolder(PathJoin(Papp.DataPath, "storage"))
+	sharedPath := utl.CreateFolder(app.DataPath, "shared")
+	storagePath := utl.CreateFolder(app.DataPath, "storage")
 
-	postInstallGit := PathJoin(Papp.AppPath, "git", "post-install.bat")
+	postInstallGit := utl.PathJoin(app.AppPath, "git", "post-install.bat")
 	if _, err := os.Stat(postInstallGit); err == nil {
-		Log.Info("Initializing git...")
+		Log.Info().Msg("Initializing git...")
 		if err = proc.QuickCmd("cmd", []string{"/k", postInstallGit}); err != nil {
-			Log.Errorf("Cannot initialize git: %v", err)
+			Log.Fatal().Err(err).Msg("Cannot initialize git")
 		}
 	}
 
-	Log.Info("Setting machine environment...")
-	OverrideEnv("MACHINE_NAME", cfg.Machine.Name)
-	OverrideEnv("MACHINE_HOST_CIDR", cfg.Machine.HostCIDR)
-	OverrideEnv("MACHINE_CPU", strconv.Itoa(cfg.Machine.CPU))
-	OverrideEnv("MACHINE_RAM", strconv.Itoa(cfg.Machine.Ram))
-	OverrideEnv("MACHINE_DISK", strconv.Itoa(cfg.Machine.Disk))
-	OverrideEnv("MACHINE_STORAGE_PATH", FormatUnixPath(storagePath))
-	OverrideEnv("MACHINE_SHARED_NAME", cfg.Machine.SharedName)
-	OverrideEnv("MACHINE_SHARED_PATH", sharedPath)
+	Log.Info().Msg("Setting machine environment...")
+	utl.OverrideEnv("MACHINE_NAME", cfg.Machine.Name)
+	utl.OverrideEnv("MACHINE_HOST_CIDR", cfg.Machine.HostCIDR)
+	utl.OverrideEnv("MACHINE_CPU", strconv.Itoa(cfg.Machine.CPU))
+	utl.OverrideEnv("MACHINE_RAM", strconv.Itoa(cfg.Machine.Ram))
+	utl.OverrideEnv("MACHINE_DISK", strconv.Itoa(cfg.Machine.Disk))
+	utl.OverrideEnv("MACHINE_STORAGE_PATH", utl.FormatUnixPath(storagePath))
+	utl.OverrideEnv("MACHINE_SHARED_NAME", cfg.Machine.SharedName)
+	utl.OverrideEnv("MACHINE_SHARED_PATH", sharedPath)
 
-	Log.Info("Adding app to PATH...")
-	OverrideEnv("PATH", fmt.Sprintf("%s;%s", Papp.AppPath, os.Getenv("PATH")))
+	Log.Info().Msg("Adding app to PATH...")
+	utl.OverrideEnv("PATH", fmt.Sprintf("%s;%s", app.AppPath, os.Getenv("PATH")))
 
-	Log.Info("Starting up the shell... ")
+	Log.Info().Msg("Starting up the shell... ")
 	pa := os.ProcAttr{
 		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-		Dir:   Papp.AppPath,
+		Dir:   app.AppPath,
 		Sys: &syscall.SysProcAttr{
-			CmdLine: fmt.Sprintf(` --login -i "%s"`, PathJoin(Papp.AppPath, "start.sh")),
+			CmdLine: fmt.Sprintf(` --login -i "%s"`, utl.PathJoin(app.AppPath, "start.sh")),
 		},
 	}
-	process, err := os.StartProcess(Papp.Process, []string{}, &pa)
-	if err != nil {
-		Log.Fatal(err)
-	}
-	if _, err = process.Wait(); err != nil {
-		Log.Fatal(err)
-	}
 
-	var exitArgs []string
-	if cfg.Machine.OnExitRemove {
-		exitArgs = []string{"rm", "-f", cfg.Machine.Name}
-	} else if cfg.Machine.OnExitStop {
-		exitArgs = []string{"stop", cfg.Machine.Name}
-	}
-	if len(exitArgs) > 0 {
-		if err = proc.QuickCmd("docker-machine", exitArgs); err != nil {
-			Log.Error(err)
+	defer func() {
+		var exitArgs []string
+		Log.Info().Msg("Exiting...")
+
+		if cfg.Machine.OnExitRemove {
+			exitArgs = []string{"rm", "-f", cfg.Machine.Name}
+		} else if cfg.Machine.OnExitStop {
+			exitArgs = []string{"stop", cfg.Machine.Name}
 		}
+
+		if len(exitArgs) > 0 {
+			if err := proc.QuickCmd("docker-machine", exitArgs); err != nil {
+				Log.Error().Err(err).Msg("docker-machine command error")
+			}
+		}
+	}()
+
+	process, err := os.StartProcess(app.Process, []string{}, &pa)
+	if err != nil {
+		Log.Fatal().Err(err).Msg("Process failed")
+	}
+	if _, err := process.Wait(); err != nil {
+		Log.Error().Err(err).Msg("Process failed")
 	}
 }
